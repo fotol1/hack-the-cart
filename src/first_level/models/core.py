44a1d00cc25329212686bb38e3450adf5610e305
nemexur <alex.milogradsky@gmail.com>
2022-03-26 20:16:12 +0300

    Update core

--- src/first_level/models/core.py
+++ src/first_level/models/core.py
@@ -1,24 +1,16 @@
-from typing import Dict, Union
+from typing import Dict, Any
 import numpy as np
-from pathlib import Path
 from scipy import sparse
-from dataclasses import dataclass
 
 
 class Model:
     def fit(
         self,
-        train: Union[Path, sparse.csr_matrix],
-        valid: Union[Path, sparse.csr_matrix],
+        train: sparse.csr_matrix,
+        valid: sparse.csr_matrix = None,
+        config: Dict[str, Any] = None,
     ) -> Dict[str, float]:
         raise NotImplementedError()
 
     def predict(self, data: np.ndarray) -> np.ndarray:
-        raise NotImplementedError()
-
-    def save(serialization_dir: Path) -> None:
-        raise NotImplementedError()
-
-    @classmethod
-    def load(serialization_dir: Path) -> "Model":
         raise NotImplementedError()
