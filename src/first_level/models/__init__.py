9918709468c5a43707d82ff8fc3696f9902b9dd3
nemexur <alex.milogradsky@gmail.com>
2022-03-27 10:56:24 +0300

    Add SAR to __init__.py

--- src/first_level/models/__init__.py
+++ src/first_level/models/__init__.py
@@ -1,4 +1,5 @@
 from src.first_level.models.als import ALS
+from src.first_level.models.sar import SAR
 from src.first_level.models.ease import EASE
 from src.first_level.models.slim import SLIM
 from src.first_level.models.multvae import MultVAE
